b62dcb55a2d636e258c223ae944665ffd6609798
srcwap7 <arpang@iitbhilai.ac.in>
2024-10-17 20:25:44 +0530

    committed backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const Admin = require('./schema/admin-model.js')
 
 require('dotenv').config({path:'./config.env'});
 
-const port =  process.env.PORT || 5000;
+const PORT =  process.env.PORT || 5000;
 const app=express();
 
 
